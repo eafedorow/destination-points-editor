61421c81a3963d26d3563d63144bc78720e73aad
Evgeniy Alexandrovich <61786122+eafedorow@users.noreply.github.com>
2022-08-11 17:42:03 +0300

    Login page update
    Description on login page was updated

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -17,12 +17,13 @@ export const Form = (props: Props) => {
     }
     return (
         <Paper className={s.formWrapper} elevation={6}>
-            <h1>Login</h1>
+            <h1>Авторизация</h1>
             <form className={s.form}>
                     <TextField
                         className={s.form__textField}
                         id="outlined-basic"
                         label="Логин"
+                        defaultValue="admin"
                         variant="outlined"
                         required
                     />
@@ -30,6 +31,7 @@ export const Form = (props: Props) => {
                         className={s.form__textField}
                         id="outlined-password-input"
                         label="Пароль"
+                        defaultValue="admin"
                         type="password"
                         variant="outlined"
                         required
@@ -42,7 +44,10 @@ export const Form = (props: Props) => {
                     >Вход</Button>
             </form>
             <span className={s.form__annotation}>
-                Данная версия веб-приоложения является тестовой. Для входа используйте логин и пароль: <b>admin</b> <b>admin</b>
+                Данная версия веб-приоложения является тестовой. Все приведенные точки назначения не отображают реального местоположения объектов предприятия.
+                Для входа используйте: <br/>
+                Логин: <b>admin</b> <br/>
+                Пароль: <b>admin</b> <br/>
             </span>
         </Paper>
 
